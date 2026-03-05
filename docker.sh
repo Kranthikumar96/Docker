1347a8bc07e405f44fdad7066cd8b39e6422c383
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2026-03-05 15:00:21 +0530

    docker

--- docker.sh
+++ docker.sh
@@ -5,9 +5,7 @@ dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.r
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 systemctl enable docker
-sudo usermod -aG docker ec2-user
-sudo systemctl start docker
-sudo systemctl enable docker
+usermod -aG docker ec2-user
 
 growpart /dev/nvme0n1 4
 lvextend -L +20G /dev/RootVG/rootVol
